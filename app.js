2ff180089d3d06bf2bc1856d179df9bf3369623c
Twilight <nechillebayan@gmail.com>
2021-08-27 22:32:44 +0800

    responsive font/layout

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ $( "div.contact" )
   })
   .mouseleave(function() {
     // $( this ).delay(3000).fadeIn();
-    $( this ).find( "div.hover-area" ).html( "<h2>Get in touch with me :)</h2>" );
+    $( this ).find( "div.hover-area" ).html( "<h2 id='link'>Get in touch with me :)</h2>" );
   });
